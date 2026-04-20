a078d4464b310c68e503f126d3512463faff3c9f
NolanBrefico <156413784+Nexian2@users.noreply.github.com>
2026-04-21 00:44:12 +0700

    Delete nexashop-ctf directory

--- nexashop-ctf/api/logout.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-  res.setHeader('Set-Cookie', 'nxs_sess=; HttpOnly; Path=/; Max-Age=0; SameSite=Lax');
-  return res.status(200).json({ success: true });
-};
